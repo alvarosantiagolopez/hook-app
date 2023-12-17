56e6c2de4a1d824b5dc25257446a174ae1effbfd
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-12-17 19:05:14 +0100

    AppRouter test

--- tests/09-useContext/MainApp.test.jsx
+++ tests/09-useContext/MainApp.test.jsx
@@ -1,11 +1,40 @@
 
+import { render, screen } from "@testing-library/react";
 import { MainApp } from "../../src/09-useContext/MainApp";
+import { MemoryRouter } from "react-router-dom";
 
 describe('Pruebas en <MainApp/>', () => {
 
     test('debe de mostrar el HomePage', () => {
-        render(<MainApp />)
+        render(
+            <MemoryRouter>
+                <MainApp />
+            </MemoryRouter>
+        );
 
+        expect(screen.getByText('HomePage')).toBeTruthy();
+
+    });
+
+    test('debe de mostrar el LoginPage', () => {
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                < MainApp />
+            </MemoryRouter >
+        );
+
+        expect(screen.getByText('LoginPage')).toBeTruthy();
+
+    });
+
+    test('debe de mostrar el About', () => {
+        render(
+            <MemoryRouter initialEntries={['/about']}>
+                < MainApp />
+            </MemoryRouter >
+        );
+
+        expect(screen.getByText('About')).toBeTruthy();
 
     });
 
